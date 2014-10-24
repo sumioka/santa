a9f83427d11dbd5a950ae3b59399bf71dbd410fb
kg86 <keisuke.gotou@kyushu-u.ac.jp>
2014-10-24 23:18:16 +0900

    santaが動くよ！

--- js/santa_move.js
+++ js/santa_move.js
@@ -14,22 +14,47 @@ var k_left = 37;
 var k_up = 38;
 var k_right = 39;
 var k_down = 40;
+var santa_dir = 1;
+var santaL_src = "image/santa_pack/red_l.png";
+var santaR_src = "image/santa_pack/red_r.png";
+var move_threshold = 8;
+function santamove(){
+    if (santa_dir > 0){
+        obj.attr({
+        src: santaL_src
+        });
+        santa_dir++;
+    }else if (santa_dir < 0){
+        obj.attr({
+        src: santaR_src
+        });
+        santa_dir--;
+    }
+    if (santa_dir > move_threshold){
+        santa_dir = -1;
+    }else if (santa_dir < - move_threshold){
+        santa_dir = 1;
+    }
+    console.log(santa_dir);
+}
 
 function movePlane() {
     for (var direction in keys) {
         if (!keys.hasOwnProperty(direction)) continue;
-        if (direction == 37) {
+        if (direction == k_left) {
             obj.animate({left: "-=5"}, 0);
             // $("#plane").animate({left: "-=5"}, 0);
         }
-        if (direction == 38) {
-            obj.animate({top: "-=5"}, 0);  
+        if (direction == k_up) {
+            obj.animate({top: "-=5"}, 0);
+            santamove();
         }
-        if (direction == 39) {
+        if (direction == k_right) {
             obj.animate({left: "+=5"}, 0);  
         }
-        if (direction == 40) {
+        if (direction == k_down) {
             obj.animate({top: "+=5"}, 0);  
+            santamove();
         }
     }
 }
