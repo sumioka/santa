143450b76a23e31e500e517b54607416b11c5ec5
kg86 <keisuke.gotou@kyushu-u.ac.jp>
2014-11-20 20:39:51 +0900

    通信部分の高速化：50FPSで運営側からメッセージを送るようにした

--- public/js/unnei_communication.js
+++ public/js/unnei_communication.js
@@ -1,7 +1,13 @@
 //////////////////////////////////////////////////////////
 // 通信
 //////////////////////////////////////////////////////////
+var keys = {};
+var k_left = 37;
+var k_up = 38;
+var k_right = 39;
+var k_down = 40;
 
+var santa = "red";
 
 // 　サーバとのコネクションの作成
 var socket = io.connect(SERVER + "/unnei");
@@ -39,6 +45,75 @@ socket.on('message', function(msg) {
    }
 });
 
+function controller(){
+    // var move_keys = _communication_keys;
+    // for(var direction in keys){
+    //     if (!keys.hasOwnProperty(direction)) continue;
+    //     move_keys.red[direction] = true;
+    // }
+    // console.log(keys);
+
+    for (var direction in keys){
+        if (direction == k_left){
+		        SendMsg("message", {method:"santa_move", options:{color:santa, direction:"left"}});
+        }
+        if (direction == k_up){
+		        SendMsg("message", {method:"santa_move", options:{color:santa, direction:"up"}});
+        }
+        if (direction == k_right){
+		        SendMsg("message", {method:"santa_move", options:{color:santa, direction:"right"}});
+        }
+		    if (direction == k_down){
+		        SendMsg("message", {method:"santa_move", options:{color:santa, direction:"down"}});
+        }
+    }
+
+}
+
+
+//    $(document).keydown(function(e) {
+//    		santa = $("input[name='santa']:checked")[0].value;
+
+//    		if(santa == "non"){
+//    			return;
+//    		}
+
+// 		switch(e.keyCode){
+// 			case k_left:
+// 		        SendMsg("message", {method:"santa_move", options:{color:santa, direction:"left"}});
+// 				break;
+// 			case k_up:
+// 		        SendMsg("message", {method:"santa_move", options:{color:santa, direction:"up"}});
+// 		        break;
+// 			case k_right:
+// 		        SendMsg("message", {method:"santa_move", options:{color:santa, direction:"right"}});
+// 				break;
+// 			case k_down:
+// 		        SendMsg("message", {method:"santa_move", options:{color:santa, direction:"down"}});
+// 				break;
+// 		}
+
+//    });
+
+function init(){
+	SendMsg("message", {method:"init", options:{}});
+}
+
+function preBtn(){
+	SendMsg("message", {method:"pre", options:{}});
+}
+
+function titleBtn(){
+	SendMsg("message", {method:"title", options:{}});
+}
+
+function ruleBtn(){
+	SendMsg("message", {method:"rule", options:{}});
+}
+
+function readyGo(){
+	SendMsg("message", {method:"readyGo", options:{}});
+}
 
 // メッセージを送る
 function SendMsg(target,msg) {
@@ -48,8 +123,25 @@ function SendMsg(target,msg) {
 function DisConnect() {
   var msg = JSON.stringify({method:disconnect, options:{termId:socket.io.engine.id}});
   // メッセージを発射する
-  socket.emit('message', { value: msg });
+  socket.volatile.emit('message', { value: msg });
   // socketを切断する
   socket.disconnect();
 }
 
+$(
+    function(){
+        $(document).keydown(function(e) {
+            keys[e.keyCode] = true;
+
+            $(document).keyup(function(e) {
+                delete keys[e.keyCode];
+            });
+   	        santa = $("input[name='santa']:checked")[0].value;
+
+   	        if(santa == "non"){
+   			        return;
+   	        }
+        });
+        setInterval(controller, 20);
+    }
+);
