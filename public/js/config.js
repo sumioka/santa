328f7131bd1a3641717b904195b5a74d6d867901
sumioka <sumioka.motoshi@jp.fujitsu.com>
2014-11-23 10:57:24 +0900

    serverをherokuに変更

--- public/js/config.js
+++ public/js/config.js
@@ -1,5 +1,5 @@
 var DEBUG_LEVEL = 0; // 0: release, 1:debug
-var SERVER = "http://localhost:3000";
+var SERVER = "http://santajection.herokuapp.com";
 var SANTA_RED_GADGET = "red";
 var SANTA_BLUE_GADGET = "blue";
 var SANTA_GREEN_GADGET = "green";
